6b44311b4cd4361bd7fa487da0d3071c53ac18c1
zorgist <zorgist1609@gmail.com>
2025-05-23 21:04:19 +0300

    feat: first Python script – Hello, world!

---
+++ hello.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+a = "hello world"
+print(a)
+print("Сегодня:", datetime.now().strftime('%d.%m.%Y. %H:%M'))
---
+++ ss.py
@@ -0,0 +1,3 @@
+from datetime import datetime 
+print('Hello, world!')
+print("Сегодня:", datetime.now().strftime('%d.%m.%Y %H:%M'))
