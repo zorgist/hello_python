7097e5b0acdc0f68e289684fcf53e80cfb67d6a6
zorgist <zorgist1609@gmail.com>
2025-05-27 18:55:00 +0300

    feat: compleate logic tasks with if, for, while, and function

---
+++ python_logic/function_task.py
@@ -0,0 +1,4 @@
+def square(n):
+    return n ** 2
+num = int(input("Введите число: "))
+print(f"Квадрат числа: {square(num)}")
---
+++ python_logic/while_task.py
@@ -0,0 +1,4 @@
+password = ""
+while password != "admin":
+    password = input("Введите пароль: ")
+print("Пароль принят")
